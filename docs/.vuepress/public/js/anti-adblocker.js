40068bdf119e867fa79ecf7c22557697c492ef33
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2022-02-09 15:55:18 +0800

    Add scripts for Anti-AdBlocker

---
+++ docs/.vuepress/public/js/anti-adblocker.js
@@ -0,0 +1,45 @@
+function kiwi_geolocate(which) {
+    var ff = kiwi_isFirefox();
+    if (ff && ff <= 28) return;
+
+    var server;
+    if (which == undefined) which = (new Date()).getSeconds();
+    which = which % 3;
+    switch (which) {
+        case 0:
+            server = 'ipapi.co/json';
+            break;
+        case 1:
+            server = 'get.geojs.io/v1/ip/geo.json';
+            break;
+        case 2:
+            server = 'ifconfig.io/all.json';
+            break;
+        default:
+            break;
+    }
+
+    kiwi_ajax('https://' + server,
+        function(json) {
+            if (json.AJAX_error === undefined) {
+                console.log('GEOLOC ' + server);
+                console.log(json);
+                geoloc_json(json);
+            } else {
+                if (geo.retry++ < 3) {
+                    kiwi_geolocate(which + 1);
+                } else {
+                    console.log('AdBlock enabled, using other APIs...');
+                    kiwi_ajax('https://api.ip.sb/geoip', function(json) {
+                        json = {
+                            city: json.organization,
+                            country: json.country
+                        };
+                        console.log(json);
+                        geoloc_json(json);
+                    }, null, 5000);
+                }
+            }
+        }, null, 5000
+    );
+}
